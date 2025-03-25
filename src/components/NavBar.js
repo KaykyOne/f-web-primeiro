586b6995b312ac448281b71cf013f7c56c440bd9
KaykyOne <kaykyzioti@gmail.com>
2025-03-25 16:45:25 -0300

    lancamento pro github

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,19 +7,18 @@ export default function NavBar() {
     return (
         <header className={styles.header}> {/* Usando a classe escopada */}
             <img src={logo} className={styles.logo} alt="logo" /> {/* Usando a classe escopada */}
-            
+
             {/* Usando NavLink para navegação */}
-            <NavLink className={styles.linkHeader} to="/" activeClassName={styles.active}>Home</NavLink> 
+            <NavLink className={styles.linkHeader} to="/" activeClassName={styles.active}>Home</NavLink>
             <NavLink className={styles.linkHeader} to="/about" activeClassName={styles.active}>Sobre</NavLink>
 
             <div className={styles.headerButtons}> {/* Usando a classe escopada */}
-                <button>Enviar Mensagem 
-                    <span className="material-symbols-outlined">
-                        call
-                    </span>
+                <button>Área do Cliente
+                    <span className="material-symbols-outlined">account_circle</span>
                 </button>
 
             </div>
         </header>
     );
 }
+
